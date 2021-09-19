99b7ab25d7fb75382bcd5332cb740c4834420678
Антон <sierra_93@mail.ru>
2021-09-19 15:11:28 +0300

    Проверка тимсити v1.0.

--- Garant.Platform/Startup.cs
+++ Garant.Platform/Startup.cs
@@ -1,19 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using System.Net;
 
 namespace Garant.Platform
 {
@@ -31,16 +21,6 @@ namespace Garant.Platform
         {
 
             services.AddControllers();
-            //services.Configure<ForwardedHeadersOptions>(options =>
-            //{
-            //    options.KnownProxies.Add(IPAddress.Parse("194.67.116.58"));
-            //});
-            //services.AddControllers(mvcOptions =>
-            //{
-            //    mvcOptions.EnableEndpointRouting = false;
-            //});
-            //services.Configure<KestrelServerOptions>(
-            //    Configuration.GetSection("Kestrel"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Garant.Platform", Version = "v1" });
@@ -67,7 +47,6 @@ namespace Garant.Platform
             {
                 endpoints.MapControllers();
             });
-            //app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Garant.Platform v1"));
             app.UseEndpoints(endpoints =>
