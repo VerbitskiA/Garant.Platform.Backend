7b9e393143acdc5ebf87479476af16bd53e90940
Антон <sierra_93@mail.ru>
2021-09-20 23:15:55 +0300

    Проверка кестрела v2.1

---
+++ Garant.Platform/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Garant.Platform.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class BaseController : Controller
+    {
+        [Route("gettest")]
+        public IActionResult GetTest()
+        {
+            return Ok();
+        }
+    }
+}
