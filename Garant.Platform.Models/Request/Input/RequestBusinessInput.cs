17f5fb0de5035481d99834e56f800552ac267973
Антон <sierra_93@mail.ru>
2021-11-29 23:19:22 +0300

    Завел сущность (не доделана) для заявок бизнеса.

---
+++ Garant.Platform.Models/Request/Input/RequestBusinessInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Garant.Platform.Models.Request.Input
+{
+    /// <summary>
+    /// Класс входной модели создания заявки бизнеса.
+    /// </summary>
+    public class RequestBusinessInput
+    {
+    }
+}
